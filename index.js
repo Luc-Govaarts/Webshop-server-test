fe786b41cd2f324f4114acbef8f93f4387a9e5ab
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-06-12 10:19:02 +0200

    Procfile added on release

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const authRouter = require("./routers/auth");
 const orderRouter = require("./routers/order");
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
